1051550cbbe8b56999fe2bd9cf5faa7be66aae53
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-12-07 17:52:43 -0500

    Setup project and pull data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	useEffect(
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		async () => {
+			const response = await fetch(
+				'https://api.covidtracking.com/v1/us/current.json'
+			);
+
+			console.log('response', response);
+
+			const data = await response.json();
+
+			console.log('data', data);
+
+			return () => {
+				console.log('useEffect done');
+			};
+		},
+		[]
+	);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>COVID Dashboard</h1>
+			</header>
+		</div>
+	);
+};
 
 export default App;
